df6aea27027d7543f2ac919e23ec5c8fea80867e
swastika-2070 <swstikasri.04@gmail.com>
2026-09-03 16:35:20 +0530

    Register root API controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { PrismaModule } from './prisma/prisma.module.js';
 import { ProductsModule } from './products/products.module.js';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 
 @Module({
   imports: [PrismaModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
